cc6b082f723b321e170ec7d2b67d12778d1fcac7
Eric D. Helms <ericdhelms@gmail.com>
2013-12-16 10:57:59 -0500

    Initial commit.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class candlepin {
   Exec { logoutput => true, timeout => 0 }
 
-  include postgres
+  include postgresql
   include candlepin::params
   include candlepin::config
   include candlepin::service
